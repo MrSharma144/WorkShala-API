90f30d91fec37a3c9ecc9b513b7903d2f4e9fa88
MrSharma144 <prateekps0123@gmail.com>
2023-11-09 18:11:39 +0530

    Add the User's account activation and Swagger documentation

--- django_api/api/views.py
+++ django_api/api/views.py
@@ -8,7 +8,8 @@ from .models import User
 from .utils import Util
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
-
+import jwt
+from django.conf import settings
 
  #Create your views here.
 class RegisterView(generics.GenericAPIView):
@@ -41,5 +42,20 @@ class RegisterView(generics.GenericAPIView):
     
 
 class VerifyEmail(generics.GenericAPIView):
-    def get(self):
-        pass
+    def get(self,request):
+        token = request.Get.get('token')
+        try:
+            payload=jwt.decode(token, settings.SECRET_KEY)
+            user=User.objects.get(id=payload['user_id'])
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+
+            return Response({'email': 'successfully Activated'},status=status.HTTP_200_OK)
+        
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({'error': 'Activation Expired'},status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'error': 'Invalid Token'},status=status.HTTP_400_BAD_REQUEST)
+
+        
