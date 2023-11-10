2965f3265bc75b9ae19b4ac18f56dd40547ad017
MrSharma144 <prateekps0123@gmail.com>
2023-11-10 17:31:50 +0530

    Swagger Documentation Added for Rest API

--- django_api/api/views.py
+++ django_api/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.response import responses
 from django.shortcuts import render
 from rest_framework.response import Response
-from .serializers import RegisterSerlizer
-from rest_framework import generics,status
+from .serializers import RegisterSerlizer,EmailVerificationSerializer
+from rest_framework import generics,status,views
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
 from .utils import Util
@@ -10,6 +10,8 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
 import jwt
 from django.conf import settings
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
  #Create your views here.
 class RegisterView(generics.GenericAPIView):
@@ -32,7 +34,7 @@ class RegisterView(generics.GenericAPIView):
         current_site= get_current_site(request).domain
         relativeLink=reverse('email-verify')
         
-        absurl='http://'+current_site+relativeLink+'?token'+str(token)
+        absurl='http://'+current_site+relativeLink+'?token='+str(token)
         email_body='Hi'+user.username+'Use link below to verify your email \n'+ absurl
         data = {'email_body':email_body, 'to_email': user.email, 'email_subject': 'Verify your email'}
         
@@ -41,9 +43,13 @@ class RegisterView(generics.GenericAPIView):
         return Response(user_data,status=status.HTTP_201_CREATED)
     
 
-class VerifyEmail(generics.GenericAPIView):
-    def get(self,request):
-        token = request.Get.get('token')
+class VerifyEmail(views.APIView):
+    serializer_class=EmailVerificationSerializer
+    token_param_config = openapi.Parameter('token', in_=openapi.IN_QUERY, description='Desription',type=openapi.TYPE_STRING)
+
+    @swagger_auto_schema(manual_parameters=[token_param_config])
+    def get(self, request):
+        token = request.GET.get('token')
         try:
             payload=jwt.decode(token, settings.SECRET_KEY)
             user=User.objects.get(id=payload['user_id'])
