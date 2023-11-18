e44059c6d3af2487414f0cdffe2c0258c8e90d0d
Prateek Sharma <119415384+MrSharma144@users.noreply.github.com>
2023-11-18 15:11:25 +0530

    Update urls.py

--- django_api/api/urls.py
+++ django_api/api/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('register/',RegisterView.as_view(),name='register'),
     path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
     path('login/',LoginAPIView.as_view(),name= 'login'),
-    path('profiles/<str:user__username>/', ProfileListCreateAPIView.as_view(), name='profile-list-create'),
+    path('profiles/', ProfileListCreateAPIView.as_view(), name='profile-list-create'),
     path('profiles/<str:user__username>/', ProfileDetailAPIView.as_view(), name='profile-detail'),
 ]
