fce43d15b8a3c96cd734dd1543824ad5804a9bc5
Prateek Sharma <119415384+MrSharma144@users.noreply.github.com>
2024-11-06 09:55:46 +0530

    Change the django Authorization

--- django_api/django_api/settings.py
+++ django_api/django_api/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2s_csjv=!lmp&-18x$n-98-g)lw@nq)o3j1yue#3dt3mr*q6-*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "workshala-api.onrender.com"]
 
